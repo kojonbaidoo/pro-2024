fc2ea28a0f980239195fe6177fd03e72c10a1938
inspector <pknb21@gmail.com>
2024-04-12 20:08:52 +0100

    round 1

--- trader.py
+++ trader.py
@@ -1,6 +1,7 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
+import jsonpickle
 
 class Trader:
     def run(self, state: TradingState):
@@ -9,95 +10,105 @@ class Trader:
 
         # Orders to be placed on exchange matching engine
         result = {}
+        my_trader_data = {"STARFRUIT":[], "AMETHYSTS":[]}
+
+        if len(state.traderData) != 0:
+            my_trader_data = jsonpickle.decode(state.traderData)
+
         for product in state.order_depths:
             print("PRODUCT: ",product)
             order_depth: OrderDepth = state.order_depths[product]
             try: position: int = state.position[product]
             except: position = 0
-            # if product == "AMETHYSTS": result[product] = self.handleAmethysts(order_depth=order_depth, product=product, position=position)
-            if product == "STARFRUIT": result[product],state.traderData = self.handleStarfruit(order_depth=order_depth, product=product, position=position, state=state)
+            if product == "AMETHYSTS": result[product] = self.handleAmethysts(order_depth=order_depth, product=product, position=position)
+            if product == "STARFRUIT":
+                result[product],my_trader_data["STARFRUIT"] = self.handleStarfruit(order_depth=order_depth, product=product, position=position, mid_price_ma_info=my_trader_data["STARFRUIT"])
             
 
         # String value holding Trader state data required. 
         # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
+        traderData = jsonpickle.encode(my_trader_data) 
 
         # Sample conversion request. Check more details below. 
         conversions = 1
         return result, conversions, traderData
 
     def handleAmethysts(self, order_depth: OrderDepth, product: string, position: int):
-        LIMIT = 20
+        LIMIT = 10
         print("POSITION: ",position)
         
         orders: List[Order] = []
         acceptable_price = 10000  # Participant should calculate this value
+        acceptable_sell = 10002
+        acceptable_buy = 9998
         # print("Acceptable price : " + str(acceptable_price))
         print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
-        max_buy_amount = LIMIT - position
-        max_sell_amount = LIMIT + position 
+        max_buy_amount = min(LIMIT - position, LIMIT)
+        max_sell_amount = min(LIMIT + position, LIMIT) 
 
-        if len(order_depth.buy_orders) != 0:
-            best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-            print("Best bid: ",best_bid)
-            if int(best_bid) > acceptable_price:
-                bid_amount = min(best_bid_amount, max_sell_amount)
-                print("SELL", str(bid_amount) + "x", best_bid)
-                orders.append(Order(product, best_bid, -bid_amount))
 
-        if len(order_depth.sell_orders) != 0:
-            best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-            print("Best ask: ",best_ask)
-            if int(best_ask) < acceptable_price:
-                ask_amount = min(best_ask_amount,max_buy_amount)
-                print("BUY", str(-ask_amount) + "x", best_ask)
-                orders.append(Order(product, best_ask, -ask_amount))
+        best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+        print("Best bid: ",best_bid)
+
+        bid_amount = min(best_bid_amount, max_sell_amount)
+        print("SELL", str(max_sell_amount) + "x", acceptable_sell)
+        orders.append(Order(product, acceptable_sell, -max_sell_amount))
+
+
+        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+        print("Best ask: ",best_ask)
+
+        ask_amount = min(best_ask_amount,max_buy_amount)
+        print("BUY", str(max_buy_amount) + "x", acceptable_buy)
+        orders.append(Order(product, acceptable_buy, max_buy_amount))
 
         return orders
  
-    def handleStarfruit(self, order_depth: OrderDepth, product: string, position: int, state: TradingState):
+    def handleStarfruit(self, order_depth: OrderDepth, product: string, position: int, mid_price_ma_info: List):
         LIMIT = 20
+        MA_DAYS = 6
         print("POSITION: ",position)
         
-        mid_price_ma_info = list(state.traderData.split(","))
-        if len(mid_price_ma_info) < 2: mid_price_ma_info = [5000, 25000]
-        else: mid_price_ma_info = [int(item) for item in mid_price_ma_info]
-
-        acceptable_price = int(mid_price_ma_info[1])/5  # Participant should calculate this value
-
         orders: List[Order] = []
         mid_price: int = 0
 
-        print("Acceptable price : " + str(acceptable_price))
         print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
         max_buy_amount = LIMIT - position
-        max_sell_amount = LIMIT + position 
+        max_sell_amount = LIMIT + position
+
+        best_bid, best_bid_amount = None, None
+        best_ask, best_ask_amount = None, None
 
         if len(order_depth.buy_orders) != 0:
             best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-            mid_price+=best_bid
-
+            mid_price += best_bid * 0.5
             print("Best bid: ",best_bid)
-            if int(best_bid) > acceptable_price:
-                bid_amount = min(best_bid_amount, max_sell_amount)
-                print("SELL", str(bid_amount) + "x", best_bid)
-                orders.append(Order(product, best_bid, -bid_amount))
-
+            
         if len(order_depth.sell_orders) != 0:
             best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-            mid_price += best_ask
-
+            mid_price += best_ask * 0.5
             print("Best ask: ",best_ask)
+            
+        if len(mid_price_ma_info) == MA_DAYS:
+
+            acceptable_price = sum(mid_price_ma_info) / MA_DAYS
+
+            print("Acceptable price : " + str(acceptable_price))
+
             if int(best_ask) < acceptable_price:
                 ask_amount = min(best_ask_amount,max_buy_amount)
                 print("BUY", str(-ask_amount) + "x", best_ask)
                 orders.append(Order(product, best_ask, -ask_amount))
 
-        mid_price = mid_price / 2
-        mid_price_ma_info[1] = mid_price_ma_info[1] - mid_price_ma_info[0] + mid_price
-        mid_price_ma_info[0] = mid_price
-        my_state = ",".join([str(item) for item in mid_price_ma_info])
+            if int(best_bid) > acceptable_price:
+                bid_amount = min(best_bid_amount, max_sell_amount)
+                print("SELL", str(bid_amount) + "x", best_bid)
+                orders.append(Order(product, best_bid, -bid_amount))
 
-        return orders,my_state
+            mid_price_ma_info = mid_price_ma_info[1:]
+
+        mid_price_ma_info.append(mid_price)
+
+        return orders, mid_price_ma_info
